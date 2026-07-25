8bcf0d7a11edde11ffda861ed487b811ef7a5dbc
ido.hirsh <hirshido@gmail.com>
2026-07-25 22:08:46 +0300

    docs: document public constants
    Add concise Sphinx-friendly documentation for the public control and test
    string labels so generated API docs describe their actual values accurately.

--- src/tbr/utils/constants.py
+++ src/tbr/utils/constants.py
@@ -1,11 +1,10 @@
-"""
-TBR Package Constants.
+"""TBR package constants.
 
-This module contains all constants used throughout the TBR package.
-Following Python PEP 8 guidelines for constant naming and organization.
+This module defines public string labels used by the TBR package.
 """
 
-# Standard column value names for control and test groups
-# Used in data aggregation and analysis functions
+#: Public string label for the control group.
 CONTROL_VAL = "control"
+
+#: Public string label for the test group.
 TEST_VAL = "test"
